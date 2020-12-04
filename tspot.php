f5365b09bc1a1f6b4f6bf807ba689d671ac16d2a
william1602 <57588698+william1602@users.noreply.github.com>
2020-12-04 06:14:29 +0100

    Add files via upload

--- tspot.php
+++ tspot.php
@@ -8,11 +8,10 @@ echo "Bonjour vous chercher $nspot !";
 $page1 = file_get_contents("spot.php");
 // Cherche une chaine de caractere precis
 $res = strstr($page1, $nspot);
-$ras="marée";
-$paragraphe = substr($page1, $res, $ras);
 
 
-// affiche la variable $paragraphe
-echo $paragraphe;
-unset($res);
+
+// affiche la variable $res
+echo $res;
+
  ?>
